448f3b66faec59c613ca6616a9bda83ccd01622c
colin <colin.clarke@nibrt.ie>
2021-08-11 11:51:11 +0100

    deleted_files

--- scripts/lncrna_gmap.sh
+++
@@ -1,9 +0,0 @@
-gmap_build -D ./ -d NCBI ../reference_genome/GCF_003668045.3_CriGri-PICRH-1.0_genomic.fna
-
-gmap -D ./ \
-     -d NCBI \
-     -n 1 \
-     --no-chimeras \
-     -f gff3_gene \
-     -t 32 \
-      lncrna.transcripts.fasta > gmap.gff3
