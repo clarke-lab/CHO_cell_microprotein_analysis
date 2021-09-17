f0291e5f68943f1ce54df55880d8061971cd14f7
colin <colin.clarke@nibrt.ie>
2021-09-17 14:24:31 +0100

    add gene cds counting

---
+++ scripts/calculate_gene_cds_counts.sh
@@ -0,0 +1,31 @@
+# count the CHX riboseq data
+mkdir diff_translation_analysis/reference_counts
+
+for i in nts_r1 nts_r2 nts_r3 nts_r4 ts_r1 ts_r2 ts_r3 ts_r4
+do
+cs count \
+  --count_files data/riboseq_chx/mapped/individual/$i".bam" \
+  --countfile_format BAM \
+  --fiveprime \
+  --offset 12 \
+  --min_length 28 \
+  --max_length 31 \
+  diff_translation_analysis/reference_annotation_gene.positions \
+  diff_translation_analysis/reference_counts/riboseq_$i
+done
+
+# count the RNASeq data
+for i in nts_r1 nts_r2 nts_r3 nts_r4 ts_r1 ts_r2 ts_r3 ts_r4
+do
+cs count \
+  --count_files data/rnaseq_se/mapped/individual/$i".bam" \
+  --countfile_format BAM \
+  --fiveprime \
+  ./diff_translation_analysis/reference_annotation_gene.positions \
+  ./diff_translation_analysis/reference_counts/rnaseq_$i
+done
+
+# conda deactivate
+
+# merge the RNASeq and Ribo-seq counts into a single table
+python scripts/combine_counts.py 
--- scripts/make_plastid_reference.sh
+++ scripts/make_plastid_reference.sh
@@ -11,7 +11,6 @@ diff_translation_analysis/protein_coding_reference.tmp.gtf
 awk -F'\t' '$3 == "peakfrac" {print $1}' orfrater_analysis/tid_removal_summary.txt > \
 diff_translation_analysis/peakfrac.txt 
 
-
 grep -Fv diff_translation_analysis/peakfrac.txt diff_translation_analysis/protein_coding_reference.tmp.gtf > \
 diff_translation_analysis/protein_coding_reference.gtf 
 
@@ -20,12 +19,6 @@ rm diff_translation_analysis/protein_coding_reference.tmp.gtf
 # convert to bed format
 gtf2bed --gtf diff_translation_analysis/protein_coding_reference.gtf \
 --bed diff_translation_analysis/cgr_reference.bed
-
-# old way - could be removed
-  # awk -F',' '$12 == "new" { print $0 }' orfrater_analysis/rate_regression.ncbi.csv | 
-  # awk -F',' '$27 >= 0.6 { print $0 }' | \
-  # awk -F',' '$12 >=5 { print $0 }' | \
-  # awk -F',' '{print $1}' > diff_translation_analysis/selected_novel_orfs.txt
 
 # selected novel ofs comes from R - filtered for potential false positive "new" annotation
  grep -f diff_translation_analysis/selected_novel_orfs.txt \
@@ -48,7 +41,7 @@ diff_translation_analysis/diff_translation.bed
 # use plastid to mask and generate the count reference
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate plastid
-python scripts/create_plastid_mask_reference.py
+python scripts/create_plastid_mask.py
 
 sort -k1,1 -k4,4n diff_translation_analysis/diff_translation.gtf | \
 bgzip > diff_translation_analysis/diff_translation.gtf.gz 
