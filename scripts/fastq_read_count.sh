bd36f5d2a9dc67bf79e2dcd3acd23b882e331d48
colin <colin.clarke@nibrt.ie>
2021-09-17 14:15:26 +0100

    add script for read counting

---
+++ scripts/fastq_read_count.sh
@@ -0,0 +1,41 @@
+# 1. count the raw reads
+mkdir results/read_counts
+for seqtype in riboseq_chx riboseq_harr riboseq_nd
+do
+    for f in data/$seqtype/raw_data/*fastq.gz; 
+        do echo -n "$f | "  && echo $(($(zcat $f | echo $((`wc -l`/4))))); 
+    done > results/read_counts/$seqtype.raw.counts && sed  -i '1i file | raw_read_number' results/read_counts/$seqtype.raw.counts
+done
+
+# 2. count the reads removed by trimming 
+for seqtype in riboseq_chx riboseq_harr riboseq_nd
+do
+    for f in data/$seqtype/preproccessed_data/trimmed/ribo*fastq.gz; 
+        do echo -n "$f | "  && echo $(($(zcat $f | echo $((`wc -l`/4))))); 
+    done > results/read_counts/$seqtype.trimmed.counts && sed  -i '1i file | raw_read_number' results/read_counts/$seqtype.trimmed.counts
+done
+
+# 3. count the reads remaining after filtering non-coding RNA
+for seqtype in riboseq_chx riboseq_harr riboseq_nd
+do
+    for step in rRNA_filter snoRNA_filter tRNA_filter
+    do
+        for f in data/"$seqtype"/preproccessed_data/"$step"/*Unmapped.out.mate1; 
+            do echo -n "$f | "  && echo $(($(cat $f | echo $((`wc -l`/4))))); 
+        done > results/read_counts/$seqtype.$step.counts && sed  -i '1i file | raw_read_number' results/read_counts/$seqtype.$step.counts
+    done
+done
+
+# 4. count the reads removed after preprocessing
+for seqtype in riboseq_chx riboseq_harr riboseq_nd
+do
+    for f in data/$seqtype/preproccessed_data/complete/*fastq; 
+        do echo -n "$f | "  && echo $(($(cat $f | echo $((`wc -l`/4))))); 
+    done > results/read_counts/$seqtype.final.counts && sed  -i '1i file | raw_read_number' results/read_counts/$seqtype.final.counts
+done
+
+
+
+
+
+
