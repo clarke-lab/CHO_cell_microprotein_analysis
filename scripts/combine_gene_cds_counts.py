7fc562ba116dac41c3e37beabe4f18d4651637b5
colin <colin.clarke@nibrt.ie>
2021-09-21 13:50:17 +0100

    update combine quant scripts

--- scripts/combine_gene_cds_counts.py
+++ scripts/combine_gene_cds_counts.py
@@ -17,7 +17,7 @@ sample_names = ["rnaseq_nts_r1",
                 "riboseq_ts_r4"]
 
 # load samples as DataFrames
-samples = { K : pd.read_table("diff_translation_analysis/reference_counts/%s.txt" % K,sep="\t",header=0,comment="#",index_col=None) for K in sample_names }
+samples = { K : pd.read_table("quantitation/gene/count_output/%s.txt" % K,sep="\t",header=0,comment="#",index_col=None) for K in sample_names }
 
 # combine count columns to single DataFrame
 combined_df = samples["rnaseq_nts_r1"][["region"]]
@@ -26,7 +26,7 @@ for k,v in samples.items():
 
 
 # save
-combined_df.to_csv("diff_translation_analysis/te_combined_reference_counts.txt",sep="\t",header=True,index=False,
+combined_df.to_csv("quantitation/gene/count_output/combined_counts.txt",sep="\t",header=True,index=False,
                    columns=["region","rnaseq_nts_r1","rnaseq_nts_r2","rnaseq_nts_r3","rnaseq_nts_r4",
                    "rnaseq_ts_r1","rnaseq_ts_r2","rnaseq_ts_r3","rnaseq_ts_r4", "riboseq_nts_r1","riboseq_nts_r2",
                    "riboseq_nts_r3", "riboseq_nts_r4","riboseq_ts_r1","riboseq_ts_r2","riboseq_ts_r3", "riboseq_ts_r4"])
