695792d8e49f9b8b6865a00b237a4560fdb00830
colin <colin.clarke@nibrt.ie>
2021-09-24 09:43:45 +0100

    tested

--- scripts/create_ms_fasta.sh
+++ scripts/create_ms_fasta.sh
@@ -4,33 +4,22 @@
 #### 
 #### Written by: Clarke Lab. NIBRT - colin.clarke@nibrt.ie 
 
-mkdir proteomics
-
 ### 1 Dowload the uniprot database for CGR
 uniprot_url=ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/UP000001075
-wget $uniprot_url/UP000001075_10029.fasta.gz -P proteomics && gunzip proteomics/UP000001075_10029.fasta.gz
+wget $uniprot_url/UP000001075_10029.fasta.gz -P proteomics_db && gunzip proteomics_db/UP000001075_10029.fasta.gz
 
-kent_path=/mnt/HDD2/colin/bin/kentUtils/bin/linux.x86_64
-
-
-$kent_path/bedToGenePred \
-orfrater_analysis/orfrater_predictions.reference.bed stdout | \
-
-$kent_path/genePredToGtf file stdin \
-proteomics/unfiltered_orfrater_predictions.gtf
-
-grep -f proteomics/orfs_for_proteomics.txt \
- proteomics/unfiltered_orfrater_predictions.gtf > \
- proteomics/orfrater_predictions.gtf
+grep -f orf_lists/orfs_for_proteomics.txt \
+orfrater_analysis/orfrater_predictions.reference.gtf > \
+proteomics_db/novel_orfs.gtf
 
 # convert the ORFRATER GTF output to fasta file
 agat_sp_extract_sequences.pl \
---gff proteomics/orfrater_predictions.gtf \
+--gff proteomics_db/novel_orfs.gtf \
 --fasta reference_genome/GCF_003668045.3_CriGri-PICRH-1.0_genomic.fna \
 -t cds  \
--o proteomics/orfrater_cds_protein.fa \
+-o proteomics_db/novel_orfs.fa \
 -p
 
 # join the uniprot and orfrater protein sequences 
-cat proteomics/UP000001075_10029.fasta proteomics/orfrater_cds_protein.fa > \
-proteomics/combined_reference_proteome.fa
+cat proteomics_db/UP000001075_10029.fasta proteomics_db/novel_orfs.fa > \
+proteomics_db/proteomics_db.fa
