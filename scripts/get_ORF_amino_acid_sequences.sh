0c159a2e800cd65faaf863d12a7ed2a375d56054
colin <colin.clarke@nibrt.ie>
2021-09-24 09:24:31 +0100

    tested

--- scripts/get_ORF_amino_acid_sequences.sh
+++ scripts/get_ORF_amino_acid_sequences.sh
@@ -1,21 +1,40 @@
 
+#!/bin/bash
+#### Description: output protein sequences for selected ORFs   
+####                1. Annotated ORF-RATER ORFs
+####                2. Upstream ORFs
+####                3. Non-coding ORFs
+#### Written by: NIBRT Clarke Lab. - colin.clarke@nibrt.ie
 
-grep -f proteomics/long_orfs_for_AA_freq.txt \
-orfrater_analysis/orfrater_predictions.reference.gtf > proteomics/long_orfs_for_AA_freq.gtf
+# 1. Annotated ORF-RATER ORFs
+grep -f orf_lists/long_orfs_for_AA_freq.txt \
+orfrater_analysis/orfrater_predictions.reference.gtf > amino_acid_analysis/long_orfs_for_AA_freq.gtf
 
 agat_sp_extract_sequences.pl \
---gff proteomics/long_orfs_for_AA_freq.gtf \
+--gff amino_acid_analysis/long_orfs_for_AA_freq.gtf \
 --fasta reference_genome/GCF_003668045.3_CriGri-PICRH-1.0_genomic.fna \
 -t cds  \
--o proteomics/long_orfs_for_AA_freq.fasta \
+-o amino_acid_analysis/long_orfs_for_AA_freq.fasta \
 -p 
 
-grep -f proteomics/short_orfs_for_AA_freq.txt \
-orfrater_analysis/orfrater_predictions.reference.gtf > proteomics/short_orfs_for_AA_freq.gtf
+# 2. Upstream ORFs
+grep -f orf_lists/upstream_short_orfs_for_AA_freq.txt \
+orfrater_analysis/orfrater_predictions.reference.gtf > amino_acid_analysis/upstream_short_orfs_for_AA_freq.gtf
 
 agat_sp_extract_sequences.pl \
---gff proteomics/short_orfs_for_AA_freq.gtf \
+--gff amino_acid_analysis/upstream_short_orfs_for_AA_freq.gtf \
 --fasta reference_genome/GCF_003668045.3_CriGri-PICRH-1.0_genomic.fna \
 -t cds  \
--o proteomics/short_orfs_for_AA_freq.fasta \
+-o amino_acid_analysis/upstream_short_orfs_for_AA_freq.fasta \
+-p 
+
+# 3. Non-coding ORFs
+grep -f orf_lists/non_coding_orfs.txt \
+orfrater_analysis/orfrater_predictions.reference.gtf > amino_acid_analysis/non_coding_orfs.gtf
+
+agat_sp_extract_sequences.pl \
+--gff amino_acid_analysis/non_coding_orfs.gtf \
+--fasta reference_genome/GCF_003668045.3_CriGri-PICRH-1.0_genomic.fna \
+-t cds  \
+-o amino_acid_analysis/non_coding_orfs.fasta \
 -p 
