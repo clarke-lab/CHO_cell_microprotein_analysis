cda6d3be3e05cab89bc148838757d21929118477
colin <colin.clarke@nibrt.ie>
2021-09-27 15:19:49 +0100

    updated trackplotting

--- scripts/make_coverage_tracks.sh
+++ scripts/make_coverage_tracks.sh
@@ -6,15 +6,17 @@
 ### 1 merged transcriptome tracks for new annotations 
 
 
-mkdir -p alignment_tracks/merged
-bg_dir=alignment_tracks/merged
 
-# 1. Merged Data for Figure 2B
-## Demonstrate the coverage at transcript-level
-## a) CHX full coverage
-## b) CHX p-site offset
-## c) CHX a-site offset
-## d) HARR-ND p-site
+
+# # 1. Merged Data for Figure 2B
+# ## Demonstrate the coverage at transcript-level
+# ## a) CHX full coverage
+# ## b) CHX p-site offset
+# ## c) CHX a-site offset
+# ## d) HARR-ND p-site
+
+mkdir -p alignment_tracks/merged
+merged_dir=alignment_tracks/merged
 
 ## sort and index transcriptome aligned BAMs
 for seqtype in riboseq_harr riboseq_chx riboseq_nd
@@ -27,108 +29,79 @@ do
     data/$seqtype/mapped/merged/$seqtype"Aligned.toTranscriptome.out.sorted.bam"
 done
 
-## a) CHX full coverage
+ ## a) CHX
 chx_trancript_bam_sorted=data/riboseq_chx/mapped/merged/riboseq_chxAligned.toTranscriptome.out.sort.bam
 
-bamCoverage -b $chx_trancript_bam_sorted -o $bg_dir/chx.fullcov.site.transcriptome_bs0.bedgraph \
- --outFileFormat bedgraph -bs 1 -p 70 --normalizeUsing BPM  --smoothLength 25
+# full coverage
+bamCoverage -b $chx_trancript_bam_sorted -o $merged_dir/chx.fullcov.transcriptome.bedgraph \
+  --outFileFormat bedgraph -bs 1 -p 70 --normalizeUsing BPM --smoothLength 25
 
-## b) CHX p-site offset
-mkdir -p alignment_tracks/merged
-bg_dir=alignment_tracks/merged
+## p-site
+bamCoverage -b $chx_trancript_bam_sorted -o $merged_dir/chx.p.site.transcriptome.bedgraph \
+  --outFileFormat bedgraph -bs 1 -p 70 --normalizeUsing BPM  --Offset 13
 
-## bug in deeptools: offset needs to be +1 actual (12)
-bamCoverage \
--b data/$seqtype/mapped/merged/$seqtype"Aligned.toTranscriptome.out.sorted.bam" \
--o alignment_tracks/merged/$seqtype".p.site.transcriptome.bedgraph" \
---outFileFormat bedgraph --Offset 13 -bs 1 -p 70 --normalizeUsing BPM 
-
-## c) CHX a-site offset
-bamCoverage \
--b data/$seqtype/mapped/merged/$seqtype"Aligned.toTranscriptome.out.sorted.bam" \
--o alignment_tracks/merged/$seqtype".a.site.transcriptome.bedgraph" \
---outFileFormat bedgraph --Offset 16 -bs 1 -p 70 --normalizeUsing BPM 
-
-## d) HARR-ND p-site offset
-# make bigwigs to do the substraction
-for seqtype in riboseq_harr riboseq_nd
-do
-    bamCoverage \
-    -b data/$seqtype/mapped/merged/$seqtype"Aligned.toTranscriptome.out.sorted.bam" \
-    -o alignment_tracks/merged/$seqtype".p.site.transcriptome.bigwig" \
-    --outFileFormat bigwig --Offset 13 -bs 1 -p 70 --normalizeUsing BPM 
+ ## d) HARR-ND p-site offset
+ # make bigwigs to do the substraction
+ for seqtype in riboseq_harr riboseq_nd
+ do
+     bamCoverage \
+     -b data/$seqtype/mapped/merged/$seqtype"Aligned.toTranscriptome.out.sorted.bam" \
+     -o $merged_dir/$seqtype".p.site.transcriptome.bigwig" \
+     --outFileFormat bigwig --Offset 13 -bs 1 -p 70 --normalizeUsing BPM 
 done
 
-bigwigCompare \
---bigwig1 alignment_tracks/merged/riboseq_harr.p.site.transcriptome.bigwig \
---bigwig2 alignment_tracks/merged/riboseq_nd.p.site.transcriptome.bigwig \
---outFileName alignment_tracks/merged/harr-nd.bigwig \
---outFileFormat bigwig \
---numberOfProcessors 70 \
--bs 1 \
---skipNonCoveredRegions \
---skipZeroOverZero \
---operation subtract
+ bigwigCompare \
+ --bigwig1 $merged_dir/riboseq_harr.p.site.transcriptome.bigwig \
+ --bigwig2 $merged_dir/riboseq_nd.p.site.transcriptome.bigwig \
+ --outFileName $merged_dir/merged/harr-nd.bigwig \
+ --outFileFormat bigwig \
+ --numberOfProcessors 70 \
+ -bs 1 \
+ --skipNonCoveredRegions \
+ --skipZeroOverZero \
+ --operation subtract
 
 # convert to bedgraph for R
-../bin/kentUtils/bin/linux.x86_64/bigWigToBedGraph \
-alignment_tracks/merged/harr-nd.bigwig \
-alignment_tracks/merged/harr-nd.bedgraph
+ ../bin/kentUtils/bin/linux.x86_64/bigWigToBedGraph \
+ $merged_dir/harr-nd.bigwig \
+ $merged_dir/harr-nd.bedgraph
 
+# ## 2. Individual genome coverage tracks for 
+# ##  a) RNA-seq
+# ##  b) CHX Ribo-seq Full coverage
+# ##  c) CHX Ribo-seq A-site coverage
+# ## Note: Track scaled by DESeq2 size factor
+# ## Note: Deep tools bug for offset = psite + 3=Asite=15+1(to correct bug)
 
-for seqtype in riboseq_harr riboseq_chx riboseq_nd
+mkdir -p alignment_tracks/individual/riboseq/
+mkdir -p alignment_tracks/individual/rnaseq/
+
+rnaseq_indiv_dir=alignment_tracks/individual/rnaseq/
+riboseq_indiv_dir=alignment_tracks/individual/riboseq/
+
+# set sample names
+sample_names=("nts_r1" "nts_r2" "nts_r3" "nts_r4" "ts_r1" "ts_r2" "ts_r3" "ts_r4")
+
+# DESeq2 size factors
+readarray -t rna_scale <results/section2.4/rnaseq_size_factors.txt
+readarray -t ribo_scale <results/section2.4/rnaseq_size_factors.txt
+
+for i in "${!sample_names[@]}";
 do
-    samtools sort \
-    data/$seqtype/mapped/merged/$seqtype"Aligned.toTranscriptome.out.bam" \
-    -o data/$seqtype/mapped/merged/$seqtype"Aligned.toTranscriptome.out.sorted.bam"
 
-    samtools index \
-    data/$seqtype/mapped/merged/$seqtype"Aligned.toTranscriptome.out.sorted.bam"
+# No norm, no offset, scaled by DESeq2
+    bamCoverage -b data/rnaseq_se/mapped/individual/${sample_names[i]}.bam \
+    -o $rnaseq_indiv_dir/"${sample_names[i]}".rnaseq_deseq.bw \
+    --outFileFormat bigwig -bs 1 -p 70 --normalizeUsing None  --skipNonCoveredRegions --scaleFactor ${rna_scale[i]}
 
-bamCoverage \
--b data/$seqtype/mapped/merged/$seqtype"Aligned.toTranscriptome.out.sorted.bam" \
--o alignment_tracks/merged/$seqtype".p.site.transcriptome.bedgraph" \
---outFileFormat bedgraph --Offset 13 -bs 1 -p 70 --normalizeUsing BPM 
+    bamCoverage -b data/riboseq_chx/mapped/individual/${sample_names[i]}.bam \
+    -o $riboseq_indiv_dir/"${sample_names[i]}".chx_deseq.bw \
+    --outFileFormat bigwig -bs 1 -p 70 --normalizeUsing None --skipNonCoveredRegions --scaleFactor ${ribo_scale[i]}
 
-bamCoverage \
--b data/$seqtype/mapped/merged/$seqtype"Aligned.toTranscriptome.out.sorted.bam" \
--o alignment_tracks/merged/$seqtype".a.site.transcriptome.bedgraph" \
---outFileFormat bedgraph --Offset 16 -bs 1 -p 70 --normalizeUsing BPM 
+# DESeq A-site offset for riboseq
+    
+    bamCoverage -b data/riboseq_chx/mapped/individual/${sample_names[i]}.bam \
+    -o $riboseq_indiv_dir/"${sample_names[i]}".chx_deseq_asite.bw \
+    --outFileFormat bigwig -bs 1 -p 70 --normalizeUsing None --Offset 16 --skipNonCoveredRegions --scaleFactor ${ribo_scale[i]}
+
 done
-
-
-
-chx_trancript_bam=data/riboseq_chx/mapped/merged/riboseq_chxAligned.toTranscriptome.out.bam
-chx_trancript_bam_sorted=data/riboseq_chx/mapped/merged/riboseq_chxAligned.toTranscriptome.out.sort.bam
-
-samtools sort $chx_trancript_bam -o $chx_trancript_bam_sorted
-samtools index $chx_trancript_bam_sorted
-bamCoverage -b $chx_trancript_bam_sorted -o $bg_dir/chx.fullcov.site.transcriptome.bedgraph \
- --outFileFormat bedgraph -bs 1 -p 70 --normalizeUsing BPM
-
-bamCoverage -b $chx_trancript_bam_sorted -o $bg_dir/chx.fullcov.site.transcriptome_bs2.bedgraph \
- --outFileFormat bedgraph -bs 2 -p 70 --normalizeUsing BPM
-bamCoverage -b $chx_trancript_bam_sorted -o $bg_dir/chx.fullcov.site.transcriptome_bs0.bedgraph \
- --outFileFormat bedgraph -bs 1 -p 70 --normalizeUsing BPM  --smoothLength 25
-
-# mkdir -p alignment_tracks/individual/riboseq_chx/
-# mkdir -p alignment_tracks/individual/rnaseq/
-
-# for seqtype in riboseq_chx 
-# do
-#     for replicate in  nts_r1 nts_r2 nts_r3 nts_r4 ts_r1 ts_r2 ts_r3 ts_r4
-#     do
-#         # samtools sort data/$seqtype/mapped/individual/$replicate"Aligned.toTranscriptome.out.bam" \
-#         # -o data/$seqtype/mapped/individual/$replicate"Aligned.toTranscriptome.out.sorted.bam"
-        
-#         samtools index data/$seqtype/mapped/individual/$replicate"Aligned.toTranscriptome.out.sorted.bam"
-
-        
-        
-#         bamCoverage \
-#         -b data/$seqtype/mapped/individual/$replicate"Aligned.toTranscriptome.out.sorted.bam" \
-#         -o alignment_tracks/individual/$seqtype/$replicate".coverage.bigwig" \
-#         --outFileFormat bigwig -bs 1 -p 32 --normalizeUsing BPM
-#     done
-# done 
-
